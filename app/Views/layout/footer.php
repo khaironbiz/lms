a2110c1b510b912891d703da8ccd197124c9c1de
KhaironStudent <khairon.khairon11078@student.binawan.ac.id>
2022-03-10 16:13:54 +0700

    bs5

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -82,7 +82,10 @@ $menu_layanan = $menu->layanan();
 
   <!-- Vendor JS Files -->
   <script src="<?= base_url() ?>/assets/template/assets/vendor/aos/aos.js"></script>
-  <script src="<?= base_url() ?>/assets/template/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- bootstrap 5 -->
+  <script src="<?= base_url() ?>/assets/template/assets/vendor/bootstrap/js/bs5.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
+
   <script src="<?= base_url() ?>/assets/template/assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="<?= base_url() ?>/assets/template/assets/vendor/php-email-form/validate.js"></script>
   <script src="<?= base_url() ?>/assets/template/assets/vendor/purecounter/purecounter.js"></script>
